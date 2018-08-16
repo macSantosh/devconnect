58901c474a23173591428d2f026df85a712eba72
macSantosh <smchoudhary123@gmail.com>
2018-08-16 00:30:45 -0400

    user register and login module committed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 
-//@route GET api/posts/test
+//load user model
+const User = require("../../models/User");
+
+//@route GET api/users/test
 //@desc Tests users route
 //@access Public
 router.get("/test", (req, res) =>
@@ -9,5 +14,80 @@ router.get("/test", (req, res) =>
     msg: "users success"
   })
 );
+
+//@route GET api/users/register
+//@desc Regsiter the user
+//@access Public
+router.post("/register", (req, res) => {
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (user) {
+      //if user already exist throw error 400
+      return res.status(400).json({ email: "email already exists" });
+    } else {
+      //if new user, create new user
+      const avatar = gravatar.url(req.body.email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar, //try giving avator:avator
+        password: req.body.password
+      });
+
+      // bcrypt.genSalt(10, (err, salt) => {
+      //   bcrypt.hash(newUser.password, salt, (err, hash) => {
+      //     if (err) {
+      //       throw err;
+      //     }
+      //     newUser.password = hash;
+      //     newUser
+      //       .save()
+      //       .then(user => res.json(user))
+      //       .catch(err => {
+      //         console.log(err);
+      //       });
+      //   });
+      // }); //suggested by tutorial
+
+      bcrypt.hash(newUser.password, 10, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser
+          .save()
+          .then(user => {
+            res.json(user);
+          })
+          .catch(err => console.log(err));
+      });
+    }
+  });
+});
+
+//@route GET api/users/login
+//@desc login user / returning JWT token
+//@access Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //find user by email
+  User.findOne({ email: email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "success" });
+      } else {
+        return res.status(400).json({ password: "password incorrect" });
+      }
+    });
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+
 const dbConnect = require("./mongodbConnect");
 
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("hello ");
